51534e88633194b5349d2f0be275aef2951c3c30
Oleg Lalykin <o.lalykin@yadro.com>
2022-10-25 11:27:38 +0300

    shift arg comments

--- HistMemAddr/accHist.py
+++ HistMemAddr/accHist.py
@@ -4,16 +4,20 @@ import matplotlib.pyplot as plt
 import sys
 
 class Histogram:
-    def __init__(self, filename, access_size=4, measure=' Kb', first_skip_access_count=0, last_skip_access_count=0, extremum_size=-1):
-        self.filename = filename
-        self.address_count = 0
-        self.access_size = access_size
-        self.measure = measure
-        self.first_skip_access_count = first_skip_access_count
-        self.last_skip_access_count = last_skip_access_count
+    def __init__(self, filename, access_size = 4, measure=' Kb', first_skip_access_count=0, last_skip_access_count=0, extremum_size=-1):
+        self.filename = filename # file with raw memory addresses, filename = <prefix>-<testname>
+        self.testInfo = self.filename.split('-')
+        if len(self.testInfo) < 2:
+            print("incorrect filename template, please use: <prefix>-<testname>")
+            exit()
+        self.access_size = access_size # count of measure associated with 1 memory access on OX
+        self.measure = measure # unit of measure for the amount of memory on OX
+        self.first_skip_access_count = first_skip_access_count # number of first memory accesses to skip while drawing
+        self.last_skip_access_count = last_skip_access_count # number of recent memory accesses to skip while drawing
+        self.extremum_size = extremum_size # point on OX for drawing straight lines around extremum, should be a multiple of access_size value
+        self.address_count = 0 
         self.linspace_left = 0
         self.linspace_right = 0
-        self.extremum_size = extremum_size
 
     def GetArray(self):
         with open(self.filename, 'r') as f:
@@ -47,19 +51,19 @@ class Histogram:
 
     def PlotHistogram(self, lines=True, points=False, xscale=True, yscale=False, extremum=False): 
         accesses = self.GetAddrAccess()       
-        address_bytes = np.arange(self.linspace_left, self.linspace_right , self.access_size)
+        address_sizes = np.arange(self.linspace_left, self.linspace_right , self.access_size)
 
         print("The len(accesses) is: ", len(accesses))
-        print("The len(address_bytes) is: ", len(address_bytes))
-        print("The address_bytes is: ", address_bytes)
+        print("The len(address_sizes) is: ", len(address_sizes))
+        print("The address_sizes is: ", address_sizes)
 
         fig = plt.figure(figsize = (10, 5))
         if points:
             # create points histogram
-            plt.plot(address_bytes, accesses, 'ro', markersize=1)   
+            plt.plot(address_sizes, accesses, 'ro', markersize=1)   
         if lines:
             # create max lines histogram
-            plt.plot(address_bytes, accesses, linewidth=0.5)
+            plt.plot(address_sizes, accesses, linewidth=0.5)
 
         if xscale:
             plt.xscale('log')        
@@ -76,27 +80,25 @@ class Histogram:
 
         if extremum and extremum_size != -1:
             size_point = self.extremum_size
-            accesses_point = accesses[np.where(address_bytes==size_point)]
+            accesses_point = accesses[np.where(address_sizes==size_point)]
             plt.axvline(x=size_point, color='b', ls='--', lw=0.5, label='size = ' + str(size_point) + self.measure)
             plt.axhline(y=accesses_point, color='g', ls='--', lw=0.5, label='accesses = ' + str(accesses_point)[1:-1])
             plt.legend(bbox_to_anchor = (1.0, 1), loc = 'upper center')
 
         plt.xlabel('Size ('+ str(access_size) +' Kb)')
         plt.ylabel('Accesses')
-
-
-
-        testInfo = self.filename.split('-')
-        plt.title('Histogram sorted by number of accesses, \n test name = ' + str(testInfo[1]) +', raw addresses = ' + str(self.address_count))
-        fig.savefig(self.filename +'_hist.png', dpi = 300)
+        plt.title('Histogram sorted by number of accesses, \n test name = ' + str(self.testInfo[1]) +', raw addresses = ' + str(self.address_count))
+        hist_name = self.filename +'_hist.png'
+        fig.savefig(hist_name, dpi = 300)
+        print("Saving a histogram: ", hist_name)
 
 
 filename = str(sys.argv[1])
-access_size = 4 # count of Bytes for associated with 1 memory access
+access_size = 4
 measure = ' Kb'
-first_skip_access_count = 0 # count of first memory accesses for skipping (e.g. for starting from 200KB)
-last_skip_access_count = 0 # count of last memory accesses for skipping
-extremum_size = 1536 # point on OX for drawing straight lines around extremum, should be a multiple of access_size value
+first_skip_access_count = 0
+last_skip_access_count = 0
+extremum_size = 1536
 
 x = Histogram(filename, access_size, measure, first_skip_access_count, last_skip_access_count, extremum_size)
 x.PlotHistogram(lines=True, points=True, xscale=True, yscale=True, extremum=True)
